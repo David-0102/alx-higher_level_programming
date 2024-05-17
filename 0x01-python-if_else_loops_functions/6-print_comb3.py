bb3fa1f3f87f6156451e2762813431fa9ed20e76
David-0102 <dmumondunda@gmail.com>
2024-05-18 02:05:52 +0300

    6-print_comb3.py

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
+# Loop through the first digit from 0 to 9
 for i in range(10):
+    #loop through the second digit from i+1 to 9 to ensure unique combinations
     for j in range(i + 1, 10):
+        # Print combinations with a comma and space, except for the last combination
         if i < 8 or j < 9:
             print("{:02d}, ".format(i * 10 + j), end="")
         else:
             print("{:02d}".format(i * 10 + j))
-
