ae0c4f22a5c9bad9582197a9464878afeb2b76c8
David-0102 <davidmumondunda@gmail.com>
2024-07-30 17:18:46 -0700

    0-readme.js

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,8 +1,8 @@
-#!/usr/bin/node
+#!/usr/bin/env node
 
 const fs = require('fs');
 
-fs.readfile(process.argv[2], 'utf8', function (err, data) {
+fs.readFile(process.argv[2], 'utf8', function (err, data) {
 	if (err) {
 		console.log(err);
 	} else {
